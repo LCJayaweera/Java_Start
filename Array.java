4187c0ecc8fa9e8a2493fd121eaebb7083a7f81a
Lakshan <lcjayaweera@gmail.com>
2023-07-08 08:54:36 +0900

    Mult dim Array

--- Array.java
+++ Array.java
@@ -8,20 +8,30 @@ for(int i=0;i<3;i++){
 
     for(int j=0;j<4;j++){
 
-        num[i][j]=(int)(Math.random()*100);
+        num[i][j]=(int)(Math.random()*10);
     }
 }
 
 
 
-for(int i=0;i<3;i++){
+// for(int i=0;i<3;i++){
 
-    for(int j=0;j<4;j++){
+//     for(int j=0;j<4;j++){
 
-        System.out.print(num[i][j]+" ");
+//         System.out.print(num[i][j]+" ");
+//     }
+//     System.out.println();
+// }
+
+
+for (int n[]:num){
+    for(int m:n){
+
+        System.out.print(m+" " );
     }
     System.out.println();
 }
+
 
 
 }
