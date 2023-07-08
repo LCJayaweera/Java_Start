01a465f09f7848c2758e1c8df9750f965dd52d94
Lakshan <lcjayaweera@gmail.com>
2023-07-08 18:36:55 +0900

    String buffer functions

--- Strbffer.java
+++ Strbffer.java
@@ -2,9 +2,20 @@ public class Strbffer {
 
 public static void main(String a[]){
     StringBuffer sb =new StringBuffer("Lakshan");
-    System.out.println(sb);
+   
 
-    System.out.println(sb.capacity());
+    // System.out.println(sb.capacity());
+    // System.out.println(sb.length());
+
+sb.append(" Jayaweera");
+sb.insert(6," Chathuranga");
+
+ System.out.println(sb);
+
+sb.setLength(45);
+
+ System.out.println(sb.length());
+
 }
     
 }
