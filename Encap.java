b26b6ac7f83b04950328af45ddfa1c8156179025
Lakshan <lcjayaweera@gmail.com>
2023-07-08 21:03:31 +0900

    Encap set and get funtion

--- Encap.java
+++ Encap.java
@@ -1,6 +1,15 @@
 class Human{
-    private String name="Lakshan";
-    private int age=32;
+    private String name;
+    private int age;
+
+public void getAge(int a){
+    age=a;
+}
+
+public void getName(String b){
+    name=b;
+}
+
 
 public int setAge(){
     return age;
@@ -17,7 +26,8 @@ public String setName(){
 public class Encap {
 public static void main(String a[]){
     Human person = new Human();
-
+person.getAge(32);
+person.getName("Lakshan");
     System.out.println(person.setAge()+" : "+person.setName());
 }    
 }
