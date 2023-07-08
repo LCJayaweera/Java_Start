ffe312d6e1b2efa1cfac0f06d97a762e8b9280fb
Lakshan <lcjayaweera@gmail.com>
2023-07-08 19:24:21 +0900

    Class.forName()

--- StatBlok.java
+++ StatBlok.java
@@ -29,23 +29,24 @@ public class StatBlok {
 
 public static void main(String a[]){
 
-Mobile obj1 = new Mobile();
-Mobile obj2 = new Mobile();
-Mobile obj3 = new Mobile();
+// Mobile obj1 = new Mobile();
+// Mobile obj2 = new Mobile();
+// Mobile obj3 = new Mobile();
 
 
 
 
-
-obj1.color = "Black";
-obj2.color = "Red";
-obj3.color = " White";
-
+// obj1.color = "Black";
+// obj2.color = "Red";
+// obj3.color = " White";
 
 
-obj1.show();
-obj2.show();
-obj3.show();
+
+// obj1.show();
+// obj2.show();
+// obj3.show();
+
+Class.forName("Mobile");//get error
 }
 
 
