e77ad620e8f743af2126ef3a44b03425734da9ef
Lakshan <lcjayaweera@gmail.com>
2023-07-08 14:53:23 +0900

    String init,print

---
+++ Strbffer.java
@@ -0,0 +1,10 @@
+public class Strbffer {
+
+public static void main(String a[]){
+    StringBuffer sb =new StringBuffer("Lakshan");
+    System.out.println(sb);
+
+    System.out.println(sb.capacity());
+}
+    
+}
