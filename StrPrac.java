29fa7cf824e61522f554958036a347b1d342efec
Lakshan <lcjayaweera@gmail.com>
2023-07-08 12:33:48 +0900

    String init,print

---
+++ StrPrac.java
@@ -0,0 +1,14 @@
+public class StrPrac {
+
+
+ public static void main(String a[]){
+    String name =new String("Lakshan");
+
+    System.out.println("hello "+name);
+    System.out.println(name.charAt(2));
+ }
+ 
+ 
+ 
+    
+}
